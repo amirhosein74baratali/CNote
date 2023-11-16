f03b42f958ad31ba5a39fb17cb995e4f0f5dd71d
amirhosein <ah.baratali74@gamil.com>
2023-11-16 13:05:16 +0330

    Initial Commit

---
+++ app/src/main/java/com/example/cnote/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.cnote
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.cnote.ui.theme.CNoteTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CNoteTheme {
+
+            }
+        }
+    }
+}
